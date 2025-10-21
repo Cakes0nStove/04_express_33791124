4f29aeee280eac5ada2aa8ffe1404401aad4f0e1
Hcosk <hakancoskun824@gmail.com>
2025-10-21 20:46:58 +0100

    uploading local work onto a public repo

---
+++ index.js
@@ -0,0 +1,18 @@
+//Imports the express module for usage in building web server.
+const express = require("express");
+//create an instance of an express application.
+const app = express();
+//defines port number where server will listen for requests.
+const port = 8000; 
+
+//loads the router handles, imports the route/main.js file,
+// which contains all the route handles for specific URLs
+const mainRoutes = require("./routes/main");
+// uses the imported routes in the express app, '/ sets the base path for these routes
+// so any route will start from the root
+app.use('/', mainRoutes);
+
+// Start listening for HTTP requests and when the server starts successfully, it sends
+// a log message to the console.
+app.listen(port, 
+    () => console.log(`Node server is running on port ${port}...`)); 
---
+++ routes/main.js
@@ -0,0 +1,42 @@
+// Create a new router
+const express = require("express");
+const router = express.Router();
+const path = require('path');
+// Handles the routes
+// Two parameters, the req(incoming request) and res (the response we send back).
+router.get("/", (req, res) => res.send("Hello World!")); 
+//handles the 'about' router and sends a html message back as a response.
+router.get("/about", (req, res) => res.send ("<h1>This is the about page</h1>"));
+//similar to about where it handles the 'contact' route and sends html response.
+router.get('/contact', (req, res) => res.send ('<h1>Contact Page: axavi001@campus.goldsmiths.ac.uk</h1>'))
+//handles the 'date' route and create a new date object with current date and time.
+router.get('/date', (req, res) => {const current_date = new Date(); 
+    //converts the date to a readable string and sends it a an HTML response.
+    res.send(current_date.toString());
+});
+router.get('/welcome/:username',(req, res) => {
+    const user = req.params.username;
+    res.send(`<h1>Welcome, ${user}!</h1>`);
+});
+// Example route with multiple callbacks
+router.get('/chain',
+  (req, res, next) => {
+    // Set a custom property on the request object
+    req.customMessage = 'Hello from the first handler!';
+    next(); // move to the next callback
+  },
+  (req, res) => {
+    // Include the custom message in the response
+    res.send(`<h1>Second function runs — response sent!</h1>
+              <p>${req.customMessage}</p>`);
+  }
+);
+router.get('/file', (req, res) => {
+  // Use path.join() to construct the correct absolute file path
+  res.sendFile(path.join(__dirname, '../a.html'));
+});
+
+
+
+// Export the router object so index.js can access it
+module.exports = router;
