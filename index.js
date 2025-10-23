719b6c38ac221f3d2a571901642a640c72e37cfc
Hcosk <hakancoskun824@gmail.com>
2025-10-23 23:35:02 +0100

    updated changes and addeding /welcome

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ const port = 8000;
 //loads the router handles, imports the route/main.js file,
 // which contains all the route handles for specific URLs
 const mainRoutes = require("./routes/main");
-app.use('/usr/278', mainRoutes);
 // uses the imported routes in the express app, '/ sets the base path for these routes
 // so any route will start from the root
-// app.use('/', mainRoutes);
+app.use('/', mainRoutes);
 
 // Start listening for HTTP requests and when the server starts successfully, it sends
 // a log message to the console.
